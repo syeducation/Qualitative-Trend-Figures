95df30170c51a1f2aec29934bbca0fc16be5e5eb
syeducation <moin@umn.edu>
2025-05-27 15:40:42 -0500

    Initial setup of environment and script.

---
+++ qual_trend_figures_script.R
@@ -0,0 +1,41 @@
+#########################################################################
+#########################################################################
+##### Reanalysis of the trend data reported in 
+##### Stecyk et al. (2025, American Psychologist) 
+##### on qualitative articles published in APA and APS journals.
+##### By Moin Syed 
+##### May 27, 2025
+#########################################################################
+#########################################################################
+
+##### Article reports trend data, but not all journals are incuded in all waves
+##### Remaking figures with only complete data
+
+library(dplyr)
+library(ggplot2)
+library(readxl)
+
+sessionInfo()
+
+# R version 4.4.2
+# RStudio 2025.05.0 Build 496
+# ggplot2_3.5.1 
+# ggplot2_3.5.1
+# dplyr_1.1.4 
+# readxl_1.4.3
+
+# read in raw data from paper, from .xlsx file
+# file has multiple sheets, we want sheet 4, Final Results
+
+dat <- readxl::read_xlsx("Comprehensive Tables - ALL RESULTS.xlsx", sheet = "Final Results")
+head(dat)
+tail(dat)
+
+# need to replace "-" with NAs
+# need to convert columns to numeric
+# need to remove final summary column
+
+
+
+
+
